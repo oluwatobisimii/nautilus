435bdcd2ded1f8bd2717dbfa603819f91863ed7d
oluwatobisimi <bamitobs@gmail.com>
2022-07-14 14:49:48 +0100

    specialization issue

--- src/views/Home.js
+++ src/views/Home.js
@@ -4,7 +4,6 @@ import FAQ from '../components/FAQ'
 import Hero from '../components/Hero'
 import { NewsLetter } from '../components/NewsLetter'
 import Partners from '../components/Partners'
-import Specialization from '../components/Specialization'
 import SpecializationNew from '../components/SpecializationNew'
 
 export const Home = () => {
