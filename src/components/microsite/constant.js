ffb639a6758d85782c9585bb2a418510fb902d55
Bamigbade Oluwatobi <bamitobs@gmail.com>
2024-10-29 17:31:27 +0100

    update package version

---
+++ src/components/microsite/constant.js
@@ -0,0 +1,33 @@
+export const testimonials = [
+    {
+        id: 1,
+        testimonial:
+            "Amazon Web Services (AWS) is an amazing public cloud infrastructure platform. At my organization, we are using AWS for all our major business services that help us to run the application and mobile applications seamlessly and smoothly. AWS provides ovide us agility to scale up and ...",
+        img: "https://randomuser.me/api/portraits/med/women/15.jpg",
+        name: "Annabelle Haugvaldstad"
+    },
+    {
+        id: 2,
+        testimonial:
+            "I had a pretty average experience with AWS maybe because many of the functions I cannot perform alone and always need a second person to help me out. I have been using this software for the last 2 months and though I learned so much about the software always I come up with new ...",
+        img: "https://randomuser.me/api/portraits/med/women/58.jpg",
+        name: "Amber Rose"
+
+    },
+    {
+        id: 3,
+        testimonial:
+            "ntegrating AWS seems next to impossible you will always need a person with solid technical knowledge of AWS and to perform the required action. Though they have good customer support at times they take way too long to reply. Even though there is plenty of information available still ... ",
+        img: "https://randomuser.me/api/portraits/med/men/86.jpg",
+        name: "Bale Dave"
+
+    },
+    {
+        id: 4,
+        testimonial:
+            "The best feature that I like the most is the AWS EC2 servers, this is really innovative and agile in nature, this can be scale up and down at any point. I can buy EC2 instances at any time and am able to add more if required, this allows us full flexibility and is very important for our overall application ...",
+        img: "https://randomuser.me/api/portraits/med/men/98.jpg",
+        name: "Perry Shawn"
+
+    },
+];
